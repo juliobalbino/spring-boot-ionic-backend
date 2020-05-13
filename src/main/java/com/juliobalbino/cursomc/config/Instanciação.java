cba5d34e0ef27f41370e4229f356b166bf1a0aeb
Julio Balbino <julio4110@hotmail.com>
2020-05-13 14:50:50 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/juliobalbino/cursomc/config/Instanciação.java
+++ src/main/java/com/juliobalbino/cursomc/config/Instanciação.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.juliobalbino.cursomc.domain.Categoria;
+import com.juliobalbino.cursomc.domain.Produto;
 import com.juliobalbino.cursomc.repositories.CategoriaRepository;
+import com.juliobalbino.cursomc.repositories.ProdutoRepository;
 
 @Configuration
 public class Instanciação implements CommandLineRunner{
@@ -15,14 +17,28 @@ public class Instanciação implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
