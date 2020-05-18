e2ea24dc4480bd9a7a5ea1c43f947a44e421c2ba
Julio Balbino <julio4110@hotmail.com>
2020-05-18 15:23:38 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/juliobalbino/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/juliobalbino/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.juliobalbino.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.juliobalbino.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 carácteres" )
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/juliobalbino/cursomc/services/CategoriaService.java
+++ src/main/java/com/juliobalbino/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.juliobalbino.cursomc.domain.Categoria;
+import com.juliobalbino.cursomc.dto.CategoriaDTO;
 import com.juliobalbino.cursomc.repositories.CategoriaRepository;
 import com.juliobalbino.cursomc.services.exceptions.DataIntegrityException;
 import com.juliobalbino.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
