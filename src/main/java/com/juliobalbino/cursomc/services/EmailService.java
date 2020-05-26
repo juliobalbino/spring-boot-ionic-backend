0cd4979bb3132065b91a26ce70187da7ceca9bd4
Julio Balbino <julio4110@hotmail.com>
2020-05-26 15:39:09 -0300

    MockEmailService com Logger. Padrões Strategy e Template

---
+++ src/main/java/com/juliobalbino/cursomc/services/EmailService.java
@@ -0,0 +1,12 @@
+package com.juliobalbino.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.juliobalbino.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+}
