147d853ef6bf374ea4d69e1189850f994facbaa1
Julio Balbino <julio4110@hotmail.com>
2020-05-28 16:44:28 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/juliobalbino/cursomc/CursomcApplication.java
+++ src/main/java/com/juliobalbino/cursomc/CursomcApplication.java
@@ -1,13 +1,24 @@
 package com.juliobalbino.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.juliobalbino.cursomc.services.S3Service;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("D:\\Projetos\\fotos teste\\images.jpg");
+	}
 }
